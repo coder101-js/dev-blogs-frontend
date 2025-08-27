a293b4f05adc4537b7f1b66fcf821e94b87d692c
='coder101_js <='op422010@gmail.com>
2025-08-27 14:26:54 +0500

    contact fetch fix

--- app/contact/page.js
+++ app/contact/page.js
@@ -22,7 +22,7 @@ const Contact = () => {
     e.preventDefault();
 
     try {
-      const res = await fetch('http://localhost:5000/contact', {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/contact`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
